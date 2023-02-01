18a3479a525772b976799f1299676d22a136d6e9
Sabrina Lingga <69828165+sabrinalingga@users.noreply.github.com>
2023-02-01 16:27:31 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello There, This is Sabby! Let's learn about GitHub!
+          Hello There, This is Sabby! 
         </p>
         <a
           className="App-link"
